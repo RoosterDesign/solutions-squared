fd1a926ad1223464d8bd1b69dd6599714500d6c7
roosterdesign <neil@roosterdesign.co.uk>
2021-03-26 08:27:56 +0000

    content blocks styling

--- wp-content/themes/solutions-squared/templates/homepage.php
+++ wp-content/themes/solutions-squared/templates/homepage.php
@@ -4,18 +4,158 @@ Template Name: Homepage
 */
 get_header(); ?>
 
+
+  <!-- Image Block -->
   <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
-    <div class="container">
-    
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/monitor.svg" alt="IT Managed Services" class="block-icon -monitor" />
-    title
-    subtitle
-    body
-    buttons
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/monitor.svg" alt="IT Managed Services" class="block-icon -monitor" />
+      <h1 class="image-block__title">IT Managed Services</h1>
+      <ul class="image-block-nav">
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Deploy<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Secure <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Support <span class="btn__chevron"></span></a>
+        </li>
 
+        <li class="lineBreak"></li>
 
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Backup <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Review <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Enhance <span class="btn__chevron"></span></a>
+        </li>
+      </ul>
     </div>
   </section>
+  <!-- End: Image Block -->
+
+
+  <!-- Text Block -->
+  <section class="text-block" id="">
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/padlock.svg" alt="Why do you need security?" class="block-icon -padlock" />
+      <h1 class="text-block__title">Why do you need security?</h1>
+      <p>The GDPR requires you to process personal data securely. This is not a new data protection obligation. It replaces and mirrors the previous requirement to have ‘appropriate technical and organisational measures’ under the Data Protection Act 1998 (the 1998 Act).</p>
+      <p>However, the GDPR provides more specifics about what you have to do about the security of your processing and how you should assess your information risk and put appropriate security measures in place. Whilst these are broadly equivalent to what was considered good and best practice under the 1998 Act, they are now a legal requirement.</p>
+    </div>
+  </section>
+  <!-- End: Text Block -->
+
+
+  <!-- Image Block -->
+  <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/info-white.svg" alt="Managed IT Security Services" class="block-icon -info" />
+      <h1 class="image-block__title">Managed IT Security Services</h1>
+      <ul class="image-block-nav">
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Antivirus<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Firewall<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Backup<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Email Filtering<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Patch Management<span class="btn__chevron"></span></a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- End: Image Block -->
+
+
+  <!-- Image Block -->
+  <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/globe-white.svg" alt="Put the web to work" class="block-icon -globe" />
+      <h1 class="image-block__title">Put the web to work</h1>
+      <ul class="image-block-nav">
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Design<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Build<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Update<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Promote<span class="btn__chevron"></span></a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- End: Image Block -->
+
+
+  <!-- Image Block -->
+  <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/drive-white.svg" alt="Cloud Services" class="block-icon -drive" />
+      <h1 class="image-block__title">Cloud Services</h1>
+      <ul class="image-block-nav">
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Email Office 365<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Sharepoint and OneDrive <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Online Backup <span class="btn__chevron"></span></a>
+        </li>
+        <li class="lineBreak"></li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Networks <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Servers <span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Firewalls <span class="btn__chevron"></span></a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- End: Image Block -->
+
+
+<!-- Image Block -->
+  <section class="image-block -hasMask" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
+    <div class="container">    
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/lightbulb-white.svg" alt="Other things we offer" class="block-icon -lightbulb" />
+      <h1 class="image-block__title">Other things we offer</h1>
+      <h2 class="image-block__sub-title">Partner Services</h2>
+      <p>Through our carefully selected partner companies we are able to extend the services that we offer to make us a truely “one stop shop” for all your IT needs. We can obtain competitive pricing on connectivity for your business, hosted telephone systems, managed print solutions and financing or lease solutions to defray the capital cost of your IT expenditure.</p>
+      <ul class="image-block-nav">
+        <li class="image-block-nav__item">
+          <a href="#" title="" class="btn">Design<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+        <a href="#" title="" class="btn">Build<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+        <a href="#" title="" class="btn">Update<span class="btn__chevron"></span></a>
+        </li>
+        <li class="image-block-nav__item">
+        <a href="#" title="" class="btn">Promote<span class="btn__chevron"></span></a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- End: Image Block -->
 
 
 
