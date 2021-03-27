fead9e0f320939916643769fe5e409e1bc68dff6
roosterdesign <neil@roosterdesign.co.uk>
2021-03-27 16:16:12 +0000

    various homepage dev

--- wp-content/themes/solutions-squared/src/js/scripts.js
+++ wp-content/themes/solutions-squared/src/js/scripts.js
@@ -1,3 +1,12 @@
+import $ from "jquery"
+import './plugins/slick-slider'
 import './components/navigation';
-// import $ from ‘jquery’
-console.log('bundle');
+
+
+$(".slider").slick({
+  autoplay: true,
+  autoplaySpeed: 6000,
+  slidesToShow: 1,
+  arrows: false,
+  dots: true
+});
--- wp-content/themes/solutions-squared/templates/homepage.php
+++ wp-content/themes/solutions-squared/templates/homepage.php
@@ -9,28 +9,28 @@ get_header(); ?>
   <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/monitor.svg" alt="IT Managed Services" class="block-icon -monitor" />
-      <h1 class="image-block__title">IT Managed Services</h1>
+      <h1 class="title -imgTitle">IT Managed Services</h1>
       <ul class="image-block-nav">
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Deploy<span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Deploy<span class="btn__chevron"></span></a>
         </li>
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Secure <span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Secure <span class="btn__chevron"></span></a>
         </li>
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Support <span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Support <span class="btn__chevron"></span></a>
         </li>
 
         <li class="lineBreak"></li>
 
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Backup <span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Backup <span class="btn__chevron"></span></a>
         </li>
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Review <span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Review <span class="btn__chevron"></span></a>
         </li>
         <li class="image-block-nav__item">
-          <a href="#" title="" class="btn">Enhance <span class="btn__chevron"></span></a>
+          <a href="#" title="" class="btn -hasChevron">Enhance <span class="btn__chevron"></span></a>
         </li>
       </ul>
     </div>
@@ -42,7 +42,7 @@ get_header(); ?>
   <section class="text-block" id="">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/padlock.svg" alt="Why do you need security?" class="block-icon -padlock" />
-      <h1 class="text-block__title">Why do you need security?</h1>
+      <h1 class="title">Why do you need security?</h1>
       <p>The GDPR requires you to process personal data securely. This is not a new data protection obligation. It replaces and mirrors the previous requirement to have ‘appropriate technical and organisational measures’ under the Data Protection Act 1998 (the 1998 Act).</p>
       <p>However, the GDPR provides more specifics about what you have to do about the security of your processing and how you should assess your information risk and put appropriate security measures in place. Whilst these are broadly equivalent to what was considered good and best practice under the 1998 Act, they are now a legal requirement.</p>
     </div>
@@ -54,7 +54,7 @@ get_header(); ?>
   <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/info-white.svg" alt="Managed IT Security Services" class="block-icon -info" />
-      <h1 class="image-block__title">Managed IT Security Services</h1>
+      <h1 class="title -imgTitle">Managed IT Security Services</h1>
       <ul class="image-block-nav">
         <li class="image-block-nav__item">
           <a href="#" title="" class="btn">Antivirus<span class="btn__chevron"></span></a>
@@ -81,7 +81,7 @@ get_header(); ?>
   <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/globe-white.svg" alt="Put the web to work" class="block-icon -globe" />
-      <h1 class="image-block__title">Put the web to work</h1>
+      <h1 class="title -imgTitle">Put the web to work</h1>
       <ul class="image-block-nav">
         <li class="image-block-nav__item">
           <a href="#" title="" class="btn">Design<span class="btn__chevron"></span></a>
@@ -105,7 +105,7 @@ get_header(); ?>
   <section class="image-block" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/drive-white.svg" alt="Cloud Services" class="block-icon -drive" />
-      <h1 class="image-block__title">Cloud Services</h1>
+      <h1 class="title -imgTitle">Cloud Services</h1>
       <ul class="image-block-nav">
         <li class="image-block-nav__item">
           <a href="#" title="" class="btn">Email Office 365<span class="btn__chevron"></span></a>
@@ -136,7 +136,7 @@ get_header(); ?>
   <section class="image-block -hasMask" id="" style="background-image: url('https://placeimg.com/1000/500/nature')">
     <div class="container">    
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/lightbulb-white.svg" alt="Other things we offer" class="block-icon -lightbulb" />
-      <h1 class="image-block__title">Other things we offer</h1>
+      <h1 class="title -imgTitle">Other things we offer</h1>
       <h2 class="image-block__sub-title">Partner Services</h2>
       <p>Through our carefully selected partner companies we are able to extend the services that we offer to make us a truely “one stop shop” for all your IT needs. We can obtain competitive pricing on connectivity for your business, hosted telephone systems, managed print solutions and financing or lease solutions to defray the capital cost of your IT expenditure.</p>
       <ul class="image-block-nav">
@@ -156,6 +156,50 @@ get_header(); ?>
     </div>
   </section>
   <!-- End: Image Block -->
+
+
+  <!-- Testimonials -->
+  <section class="testimonials-block slider">
+
+    <blockquote class="testimonial">
+      <span class="testimonial__company">[Company Name]</span>
+      <p>“Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis.”</p>
+      <span class="testimonial__author">John Snow, Managing Director.</span>
+    </blockquote>
+
+    <blockquote class="testimonial">
+      <span class="testimonial__company">[Company Name]</span>
+      <p>“Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis.”</p>
+      <span class="testimonial__author">John Snow, Managing Director.</span>
+    </blockquote>
+
+    <blockquote class="testimonial">
+      <span class="testimonial__company">[Company Name]</span>
+      <p>“Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis.”</p>
+      <span class="testimonial__author">John Snow, Managing Director.</span>
+    </blockquote>
+
+  </section>
+  <!-- End: Testimonials -->
+
+
+  <!-- News -->
+  <section class="news-block" id="news">
+    <div class="container">
+      <h1 class="title">Latest industry news</h1>
+    </div>
+  </section>
+  <!-- End: News -->
+
+  <!-- Contact -->
+  <section class="contact-block" id="contact">
+    <div class="container">
+      <h1 class="title">Have a project in mind?</h1>
+      <a href="" title="Work with us" class="btn -centre">Work with us</a>
+    </div>
+  </section>
+  <!-- End: Contact -->
+
 
 
 
